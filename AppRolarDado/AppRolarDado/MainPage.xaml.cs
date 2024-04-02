fd60018e82180510539ba80e4bd9a3891ec4056c
caioleo <caioleo>
2024-04-02 12:26:59 -0300

    Avaliação Rolar dado

---
+++ AppRolarDado/AppRolarDado/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿namespace AppRolarDado
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            int valorEscolhido = 0;
+            if (Picker.SelectedIndex == 0)
+            {
+                valorEscolhido = 4;
+            }else if (Picker.SelectedIndex == 1)
+            {
+                valorEscolhido = 6;
+            }
+            else if (Picker.SelectedIndex == 2)
+            {
+                valorEscolhido = 10;
+            }
+            else if (Picker.SelectedIndex == 3)
+            {
+                valorEscolhido = 20;
+            }
+            else if (Picker.SelectedIndex == 4)
+            {
+                valorEscolhido = 100;
+            }
+            Random numAleatorio = new Random();
+            int numero = numAleatorio.Next(1,valorEscolhido+1);
+            NumeroAleatorio.Text = numero.ToString();
+            
+        }
+    }
+
+}
